d58b1a6e4747bb4b5179c49c71a175654d763629
François Charlier <francois.charlier@enovance.com>
2012-07-10 20:41:35 +0200

    Spec tests: the package requires the user too

--- spec/classes/logio_package_spec.rb
+++ spec/classes/logio_package_spec.rb
@@ -5,13 +5,13 @@ describe 'logio::package' do
   it { should contain_package('log.io').with(
     :ensure   => 'installed',
     :provider => 'npm',
-    :require  => 'Class[Nodejs]'
+    :require  => ['Class[Nodejs]', 'User[logio]']
   ) }
 
   it { should contain_package('forever').with(
     :ensure   => 'installed',
     :provider => 'npm',
-    :require  => 'Class[Nodejs]'
+    :require  => ['Class[Nodejs]', 'User[logio]']
   ) }
 
 end
